8db6d278d47fe9889892ec1d513bda9eb01a46f1
Alejandro Aponte <yasmiverromero@gmail.com>
2026-04-29 20:27:56 -0500

    algunos bases

--- app/src/main/java/com/example/lookbyte/MainActivity.kt
+++ app/src/main/java/com/example/lookbyte/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.lookbyte
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.lookbyte.UI.MainMenu.Cliente.CarritoFragment
+import com.example.lookbyte.UI.MainMenu.Cliente.HomeFragment
+import com.example.lookbyte.UI.MainMenu.Cliente.OfertasFragment
+import com.example.lookbyte.UI.MainMenu.Cliente.PerfilFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout;
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toobal = findViewById<Toolbar>(R.id.toolbar)
+        drawerLayout= findViewById(R.id.main)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        cargarFragment(HomeFragment())
+        bottomNav.selectedItemId = R.id.nav_inicio
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_inicio -> cargarFragment(HomeFragment())
+                R.id.nav_carrito-> cargarFragment(CarritoFragment())
+                R.id.nav_ofertas -> cargarFragment(OfertasFragment())
+                R.id.nav_perfil -> cargarFragment(PerfilFragment())
+            }
+            true
+        }
+    }
+
+    private fun cargarFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
