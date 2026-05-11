277bd29e4b3533d3f638d3137c2c225659d3ef71
Nicole Moreno <0nicolemoreno1@gmail.com>
2026-05-10 20:46:43 -0500

    Home, ofertas y detalle de productos

--- app/src/main/java/com/example/lookbyte/HomeActivity.kt
+++ app/src/main/java/com/example/lookbyte/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lookbyte
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
@@ -8,5 +10,9 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+    fun abrirProducto(view: View) {
+        val intent = Intent(this, ProductActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/lookbyte/OffersActivity.kt
+++ app/src/main/java/com/example/lookbyte/OffersActivity.kt
@@ -3,10 +3,10 @@ package com.example.lookbyte
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ProductActivity : AppCompatActivity() {
+class OffersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product)
+        setContentView(R.layout.activity_offers)
     }
 }
